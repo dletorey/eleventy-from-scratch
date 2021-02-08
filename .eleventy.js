61660915d53038b14d711f3284287016a58eb8b0
Dave Letorey <dave@code-red.uk>
2021-02-08 11:54:34 +0000

    created a featured work functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 module.exports = config => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+    
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', collection => {
+      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+        x => x.data.featured
+      );
+    });
+
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
