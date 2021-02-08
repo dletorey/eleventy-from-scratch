39622a38ddfecf40de605960ff782e211319dc99
Dave Letorey <dave@code-red.uk>
2021-02-08 16:43:01 +0000

    created a blog and tag listing with pagination

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,10 @@ module.exports = config => {
         x => x.data.featured
       );
     });
+    // Returns a collection of blog posts in reverse date order
+    config.addCollection('blog', collection => {
+      return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+    });
 
     return {
         markdownTemplateEngine: 'njk',
