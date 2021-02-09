38d5a1335cf5885c02f30ad1692ad4e4f99e43ce
Dave Letorey <dave@code-red.uk>
2021-02-09 14:04:43 +0000

    created a post template and date filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 module.exports = config => {
+    // Add filters
+    config.addFilter('dateFilter', dateFilter);
+    config.addFilter('w3DateFilter', w3DateFilter);
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
     // Returns work items, sorted by display order
--- src/posts/a-complete-guide-to-wireframe-design.md
+++ src/posts/a-complete-guide-to-wireframe-design.md
@@ -2,6 +2,7 @@
 title: 'A Complete Guide to Wireframe Design'
 date: '2020-04-13'
 tags: ['Tutorial', 'Learning']
+layout: 'layouts/post.html'
 ---
 
 Placidis tamen. Amnem unda fores et nocent tellus. Ictu undis offensi nostra nempe dextra quod, illa causa expositum, dat. Cura rapta dum praepositam inhaesit, laeto sceleratus vicina utque. Annos nunc sumitur ignes ac nequit minanti.
