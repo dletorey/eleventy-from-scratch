f2cc3265ebeff54882d280c1952dbcd1a473a2e7
Dave Letorey <dave@code-red.uk>
2021-02-11 09:56:24 +0000

    created a gulpfile and gulp tasks to compile sass files

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,8 @@ module.exports = config => {
       });
     });
 
+    // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+    config.setUseGitIgnore(false);
 
     return {
         markdownTemplateEngine: 'njk',
