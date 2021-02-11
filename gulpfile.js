e98e8dde1414c0d939910ed023ed9af0f187f396
Dave Letorey <dave@code-red.uk>
2021-02-11 10:29:57 +0000

    created the gulp task to minify images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const {parallel, watch} = require('gulp');
 
 // Pull in each task
+const fonts = require('./gulp-tasks/fonts.js');
 const sass = require('./gulp-tasks/sass.js');
+const images = require('./gulp-tasks/images.js');
+
 
 // Set each directory and contents that we want to watch and
 // assign the relevant task. `ignoreInitial` set to true will
@@ -9,10 +12,11 @@ const sass = require('./gulp-tasks/sass.js');
 // will run when a file changes.
 const watcher = () => {
   watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
+  watch('./src/images/**/*', {ignoreInitial: true}, images);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parrallel
-exports.default = parallel(sass);
+exports.default = parallel(fonts, images, sass);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
